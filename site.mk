ecd2e44e4676bfca9a858997127a625931a7d430
Kaj <kst87@users.noreply.github.com>
2016-12-08 20:45:18 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,29 +1,141 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-alfred \
+	gluon-mesh-batman-adv-14 \
 	gluon-respondd \
 	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
+	gluon-config-mode-autoupdater \
 	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
 	gluon-next-node \
+	gluon-neighbour-info \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
+	ffac-status-page-api \
 	gluon-status-page \
-	haveged \
-	iptables \
 	iwinfo \
-	gluon-luci-private-wifi 
+	iptables \
+	haveged
+
+# add offline ssid only if the target has wifi device
+ifeq ($(GLUON_TARGET),ar71xx-generic)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer
+endif
+
+ifeq ($(GLUON_TARGET),ar71xx-mikrotik)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer
+endif
+
+ifeq ($(GLUON_TARGET),ar71xx-nand)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer
+endif
+
+ifeq ($(GLUON_TARGET),mpc85xx-generic)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer
+endif
+
+ifeq ($(GLUON_TARGET),ramips-rt305x)
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer
+endif
+
+# add addition network drivers and usb stuff only to targes where disk space does not matter.
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb-ohci-pci \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2 \
+	kmod-igb
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += \
+        kmod-usb-core \
+        kmod-usb-ohci-pci \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+	kmod-atl2 \
+	kmod-igb
+endif
+
+# Add offline ssid, network drivers and usb stuff to raspberry and banana pi images
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+GLUON_SITE_PACKAGES += \
+        gluon-ssid-changer \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+        kmod-atl2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+GLUON_SITE_PACKAGES += \
+        gluon-ssid-changer \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+        kmod-atl2
+endif
+
+ifeq ($(GLUON_TARGET),sunxi)
+GLUON_SITE_PACKAGES += \
+        gluon-ssid-changer \
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-dm9601-ether \
+        kmod-sky2 \
+        kmod-r8169 \
+        kmod-forcedeth \
+        kmod-8139too \
+        kmod-atl2
+endif
 
 DEFAULT_GLUON_RELEASE := 2016.2+exp$(shell date '+%Y%m%d')
 
@@ -33,8 +145,8 @@ GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_BRANCH ?= experimental
 export GLUON_BRANCH
 
-GLUON_TARGET ?= ar71xx-generic
-export GLUON_TARGET
+#GLUON_TARGET ?= ar71xx-generic
+#export GLUON_TARGET
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
