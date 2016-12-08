b602948f0e24c17c69eab4981ebd9bf6e26fe247
Kaj <kst87@users.noreply.github.com>
2016-12-08 20:55:59 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -20,7 +20,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
-	ffac-status-page-api \
+	gluon-status-page-api \
 	gluon-status-page \
 	iwinfo \
 	iptables \
